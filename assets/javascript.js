1240d5dc5ab8c9529adba5e543e69f916593df8d
Adam Sidor <adamsidor94@gmail.com>
2016-10-21 19:14:24 -0500

    weird bug

--- assets/javascript.js
+++ assets/javascript.js
@@ -5,15 +5,10 @@ var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
                "p","q","r","s","t","u","v","w","x","y","z"];
 var letterToGuess = '';
 var gameOver = false;
-var wins = 0;
-var losses = 0;
+var winCount = 0;
+var lossCount = 0;
 var guesses = 9;
 
-
-//to reset game
-function resetGame() {
-
-}
 
 
 
@@ -24,38 +19,45 @@ document.onkeyup = function (event){
 // Determines which exact key was selected. Make it lowercase
 
 
-  lettersGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+  lettersGuessed.push(String.fromCharCode(event.keyCode).toLowerCase());
 
   document.getElementById('guessesStr').innerHTML='Your guesses so far: ' + lettersGuessed;
-  lettersGuessed.appendchild() = 'Your guesses so far';
+  
 
-  }
+  
 
 //determines if correct letter guessed
 
-  var letterToGuess = true;
-  for (var i = 0; i < letterToGuess.length; i++)
+  //var letterToGuess = true;
+  //for (var i = 0; i < letterToGuess.length; i++)
 
 
 
  // Create code to randomly select letter (Computer)
   letterToGuess = letters[Math.floor(Math.random() * letters.length)];
+  console.log(letterToGuess)
 
 
+ var userGuess= 
+ String.fromCharCode(event.keyCode).toLowerCase();
+lettersGuessed.push(userGuess);
 
 
     //determines wins, losses, and guesses left
-//function psychicGame(char) {
-//if (userGuess === 'letterToGuess') {
-  // 	wins( +=1);
-   //} 
 
-//if (userGuess !== 'letterToGuess') {
-  //  guesses( -=1); 
+if (userGuess === 'letterToGuess') {
+  	winCount++
+  } 
 
-//} else if (guessCount === 0)
-//    losses( +=1);
-//}
+  else {
+    guesses--
+  }
+  
+
+ if (guesses === 0) {
+    lossCount++
+  }
+}
 
 
 
